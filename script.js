3b5064ccab612583e3f5360aea17fbe608ce5eda
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2025-09-28 17:12:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function highlight() {
-    //Write your code here
+const strongList = document.querySelectorAll("strong");
 
+function highlight(element) {
+    //Write your code here
+	strongList.forEach((s)=>{
+		s.style.color = "green";
+	})	
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	strongList.forEach((s)=>{
+		s.style.color = "black";
+	})	
     
 }
